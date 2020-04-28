02b6f6fd3dda91f98c57869f9c3e7062845b5391
MattOlson2000 <61026894+MattOlson2000@users.noreply.github.com>
2020-04-28 11:09:38 -0500

    Add files via upload

--- gui.java
+++ gui.java
@@ -1,6 +1,18 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
 
 public class gui {
 
@@ -34,8 +46,56 @@ public class gui {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setForeground(Color.WHITE);
+		frame.setBounds(100, 100, 1001, 644);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JTextArea txtrTables = new JTextArea();
+		txtrTables.setText("Tables");
+		txtrTables.setColumns(8);
+		frame.getContentPane().add(txtrTables, BorderLayout.WEST);
+		
+		JLabel lblNewLabel = new JLabel("order number");
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(10, 462, 120, 100);
+		panel.add(btnNewButton_3);
+		
+		JButton btnNewButton_3_1 = new JButton("New button");
+		btnNewButton_3_1.setBounds(10, 351, 120, 100);
+		panel.add(btnNewButton_3_1);
+		
+		JButton btnNewButton_3_2 = new JButton("New button");
+		btnNewButton_3_2.setBounds(10, 240, 120, 100);
+		panel.add(btnNewButton_3_2);
+		
+		JButton btnNewButton_3_3 = new JButton("New button");
+		btnNewButton_3_3.setBounds(10, 129, 120, 100);
+		panel.add(btnNewButton_3_3);
+		
+		JButton btnNewButton_3_4 = new JButton("New button");
+		btnNewButton_3_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_3_4.setBounds(10, 18, 120, 100);
+		panel.add(btnNewButton_3_4);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(null, "Tab", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_2.setBackground(Color.LIGHT_GRAY);
+		panel_2.setBounds(651, 11, 250, 550);
+		panel.add(panel_2);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.SOUTH);
 	}
-
 }
