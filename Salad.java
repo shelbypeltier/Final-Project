f07122b419e8508738bd70d8fb466705766abb6c
MattOlson2000 <61026894+MattOlson2000@users.noreply.github.com>
2020-05-04 10:10:20 -0500

    Add files via upload

--- Salad.java
+++ Salad.java
@@ -1,36 +1,52 @@
 
 public class Salad extends Food{
-	private int tomato,carrot,cucumber,onion;
-	private Sauce dressing;
+	private String tomato,carrot,cucumber,onion,dressing;
 	
-	public int getTomato() {
+	public Salad(double price, String name, String tomato, String carrot, String cucumber, String onion,
+			String dressing) {
+		super(price, name);
+		this.tomato = tomato;
+		this.carrot = carrot;
+		this.cucumber = cucumber;
+		this.onion = onion;
+		this.dressing = dressing;
+	}
+	public Salad() {
+		super();
+		this.tomato = null;
+		this.carrot = null;
+		this.cucumber = null;
+		this.onion = null;
+		this.dressing = null;
+	}
+	public String getTomato() {
 		return tomato;
 	}
-	public void setTomato(int tomato) {
+	public void setTomato(String tomato) {
 		this.tomato = tomato;
 	}
-	public int getCarrot() {
+	public String getCarrot() {
 		return carrot;
 	}
-	public void setCarrot(int carrot) {
+	public void setCarrot(String carrot) {
 		this.carrot = carrot;
 	}
-	public int getCucumber() {
+	public String getCucumber() {
 		return cucumber;
 	}
-	public void setCucumber(int cucumber) {
+	public void setCucumber(String cucumber) {
 		this.cucumber = cucumber;
 	}
-	public int getOnion() {
+	public String getOnion() {
 		return onion;
 	}
-	public void setOnion(int onion) {
+	public void setOnion(String onion) {
 		this.onion = onion;
 	}
-	public Sauce getDressing() {
+	public String getDressing() {
 		return dressing;
 	}
-	public void setDressing(Sauce dressing) {
+	public void setDressing(String dressing) {
 		this.dressing = dressing;
 	}
 	@Override
