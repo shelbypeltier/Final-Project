72a64cb66194f1f47913e7913e022f195f868bca
shelbypeltier <60152690+shelbypeltier@users.noreply.github.com>
2020-05-07 09:26:28 -0500

    Update Dessert.java

--- Dessert.java
+++ Dessert.java
@@ -1,18 +1,26 @@
+	public class Dessert extends Food{
+	private double priceofdessert=5.00;
 
-public class Dessert extends Food{
+		public Dessert() {
+			super();
+		}
 
-	public Dessert() {
-		super();
+		public Dessert(double price, String name) {
+			super(price, name);
+		}
+
+		@Override
+		public String toString() {
+			return "Dessert Price =" + getPrice() + "/n Name=" + getName();
+		}
+
+		public double getPriceofdessert() {
+			return priceofdessert;
+		}
+
+		public void setPriceofdessert(double priceofdessert) {
+			this.priceofdessert = priceofdessert;
+		}
+		
+		
 	}
-
-	public Dessert(double price, String name) {
-		super(price, name);
-	}
-
-	@Override
-	public String toString() {
-		return "Dessert Price =" + getPrice() + "/n Name=" + getName();
-	}
-	
-	
-}
